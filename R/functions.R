d11ce434d11272dd13f932a65bad40fc05906d3d
greghirson <ghirson@ucdavis.edu>
2010-11-30 14:50:50 -0800

    add R file

---
+++ R/functions.R
@@ -0,0 +1,25 @@
+#old faithful data to plot
+
+data(faithful)
+
+
+jsonify = function(dataframe){
+	i1 = paste((apply(dataframe, 1, toJSON)), collapse = ",")
+	i2 = paste("'[", i1, "]'", collapse = "")
+	gsub("\\\"", "", i2)
+	
+	}
+	
+
+
+RP = function(name, data, outfile = "~/Desktop/out.html"){
+	d = jsonify(data)
+	tfile = system.file("template.brew", package="RProtovis")
+	jsfile = system.file("protovis-r3.2.js", package="RProtovis")
+	#plot params to be filled in by brew
+	name = name
+	xrng = range(data[,1])
+	yrng = range(data[,2])
+	brew(tfile, outfile)
+	}
+	
